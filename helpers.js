f97bf9fd604b3ab41a354795f92e1de946659007
Ricardo Rodriguez <ricardo.rodriguez@wizeline.com>
2019-11-17 17:57:31 -0600

    Initial commit

---
+++ helpers.js
@@ -0,0 +1,22 @@
+const add = (number1, number2) => {
+  return 1;
+}
+
+const divide = (number1, number2) => {
+  return 1;
+}
+
+const multiply = (number1, number2) => {
+  return 1;
+}
+
+const substract = (number1, number2) => {
+  return 1;
+}
+
+module.exports = {
+  add,
+  divide,
+  multiply,
+  substract,
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+const {
+  add,
+  divide,
+  multiply,
+  substract,
+} = require('./helpers');
+
+// Add numbers
+console.log('Add', add(1, 2))
+// Substract numbers
+console.log('Substract', substract(1, 2))
+// Multiply numbers
+console.log('Multiply', multiply(1, 2))
+// Divide numbers
+console.log('Divide', divide(1, 2))
